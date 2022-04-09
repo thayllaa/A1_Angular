9a5183d2aeb36624f324825ad9f6a847a7e2563f
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-04-09 12:27:47 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,32 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ThayllaComponent } from './thaylla/thaylla.component';
+import { ThayllaListaComponent } from './thaylla-lista/thaylla-lista.component';
+import { ThayllaDetalhesComponent } from './thaylla-detalhes/thaylla-detalhes.component';
+import { ThayllaDisciplinasService } from './thaylla-disciplinas.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'thaylla', component: ThayllaComponent},
+      {path: 'lista', component: ThayllaListaComponent},
+      {path: 'detalhes/:index', component: ThayllaDetalhesComponent}
+    ])
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent,
+    ThayllaComponent,
+    ThayllaListaComponent,
+    ThayllaDetalhesComponent 
+  ],
+  bootstrap:    [ AppComponent ],
+  providers: [
+    ThayllaDisciplinasService
+  ]
 })
 export class AppModule { }
