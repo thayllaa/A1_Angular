e68ad91ba61f3d1d76919b835e728028d9e9c0f7
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-04-09 12:24:18 -0300

    7 files added

---
+++ src/app/thaylla-disciplinas.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+interface Disciplinas {
+  id: string;
+  name: string;
+  day: string;
+  hour: string;
+  ement: string;
+}
+
+@Injectable()
+export class ThayllaDisciplinasService {
+  lista: Array<Disciplinas> = [];
+
+  constructor() {}
+
+  getLista() {
+    return this.lista;
+  }
+
+  add(id: string, name: string, day: string, hour: string, ement: string) {
+    this.lista.push({id, name, day, hour, ement});
+  }
+
+  remove(index: number) {
+    this.lista.splice(index, 1);
+  }
+}
