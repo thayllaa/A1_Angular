95628ab804324da594537823c570fda8ba13f819
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-04-09 15:04:24 -0300

    1 file modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -14,14 +14,9 @@ export const listData = [
     ement: 'PHP histórico e emprego. Instalação e configuração básica do PHP e um IDE. Sintaxe básica do PHP. Usando o PHP como um mecanismo de modelo simples. Panorama das melhores práticas com PHP. Conceitos de programação HTTP. Codificação de caracteres. Localidades, fusos horários e funções de tempo. Strings. Uso de Array e funções de matriz. Orientação a objetos em PHP (Classes, objetos, herança, encapsulamento, polimorfismo, agregação, composição e métodos). Tratamento de exceções de erro. Arquitetura do lado do servidor. Manipulação de dados postados. Enviando e-mail. Sessões e autenticação. Cookies. Arquivo manuseio e armazenamento de dados em arquivos de texto. Gerenciador de banco de dados e suas funções. Frameworks. Web Services, API, RSS, JSON e Ajax. Hospedagem compartilhada.'
   },
   {
-    id: "",
-    name: "",
-    day: "",
-    hour: "",
     ement: 'Conceitos e evolução das tecnologias de programação de servidores. Recursos da linguagem escolhida para servidores na Internet. Padrões de projetos. Integração com sistemas (Google Maps API, Twitter, entre outros).'
   },
   {
-    
     ement: 'Introdução e conceitos. Recursos disponíveis para dispositivos móveis na linguagem de marcação de textos. Uso de algum ambiente de desenvolvimento: configuração do ambiente; emulação de dispositivos móveis nos computadores pessoais; interface gráfica; serviços disponíveis; banco de dados nos dispositivos. Desenvolvimento de aplicações que utilizam dados de geolocalização. Integração entre sistemas remotos.'
   },
   {
